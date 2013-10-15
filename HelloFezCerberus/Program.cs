f20079bcbdebb6eb79fc9fdd8fcf213fe38978fc
Steen Hulthin Rasmussen <steenhulthin@gmail.com>
2013-10-15 21:13:35 +0200

    removed unused code.

--- HelloFezCerberus/Program.cs
+++ HelloFezCerberus/Program.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections;
-using System.Threading;
-using Microsoft.SPOT;
-using Microsoft.SPOT.Presentation;
-using Microsoft.SPOT.Presentation.Controls;
-using Microsoft.SPOT.Presentation.Media;
-using Microsoft.SPOT.Presentation.Shapes;
-using Microsoft.SPOT.Touch;
-
-using Gadgeteer.Networking;
+﻿using Microsoft.SPOT;
 using GT = Gadgeteer;
-using GTM = Gadgeteer.Modules;
-using Gadgeteer.Modules.GHIElectronics;
 
 namespace HelloFezCerberus
 {
